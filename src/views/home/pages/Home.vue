496cc033fa2939a112feb2234e64585e99f02f28
Andres Romo <andres.roqa93@gmail.com>
2021-07-21 19:21:04 -0500

    First commit

---
+++ src/views/home/pages/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <ul id="commits">
+      <li v-for="(item, index) in items"
+      :key="index">
+        {{ item.mensaje }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data(){
+    return {
+      items: [
+        { mensaje: 'Foo' },
+        { mensaje: 'Bar' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
