f5d9b13ebe25766a50f358430f8472ab040a8601
Andres Romo <andres.roqa93@gmail.com>
2021-07-23 01:25:02 -0500

    add vuetify

--- src/views/home/pages/Home.vue
+++ src/views/home/pages/Home.vue
@@ -1,11 +1,10 @@
 <template>
   <div class="hello">
-    <ul id="commits">
-      <li v-for="(item, index) in commits"
-      :key="index">
-        Probando el API de gitHub
-      </li>
-    </ul>
+    <v-card v-for="(item, index) in commits" :key="index">
+      <v-card-title>{{ item.commit.author.name }}</v-card-title>
+      <v-card-subtitle>{{ item.commit.author.date }}</v-card-subtitle>
+      <v-card-text>{{ item.commit.message }}</v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -30,6 +29,7 @@ export default {
         await axios.get('https://api.github.com/repos/andrew221293/git-commit-history/commits').
         then(res => {
           if (!_.isEqual(this.commits, res.data)) {
+            console.log(res.data);
             this.commits = res.data
           }
         })
